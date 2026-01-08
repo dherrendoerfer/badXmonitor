1dafb4e8bbaf6e8120ef2722058158dc66d79b75
D.Herrendoerfer <d.herrendoerfer@herrendoerfer.name>
2026-01-08 16:47:18 +0000

    vic: more registers more color

--- examples/VIC-20/vic.c
+++ examples/VIC-20/vic.c
@@ -21,6 +21,7 @@ static uint8_t *mem;
 static uint8_t *interrupt;
 static uint16_t base_address;
 
+/*
 uint16_t palette[] = {0x0000,0x7bcf,0x0008,0x73ca,0x610c,0x2b00,0x5000,0x3b8e,0x2a0d,0x0106,0x39cf,0x18c3,0x39c7,0x33ca,0x7a00,0x5acb,
                       0x0000,0x0841,0x1082,0x18c3,0x2104,0x2945,0x3186,0x39c7,0x4208,0x4a49,0x528a,0x5acb,0x630c,0x6b4d,0x738e,0x7bcf,
                       0x0842,0x18c4,0x2106,0x3188,0x420a,0x4a4c,0x5acf,0x0842,0x1084,0x18c6,0x2108,0x294a,0x318c,0x39cf,0x0002,0x0844,
@@ -37,6 +38,32 @@ uint16_t palette[] = {0x0000,0x7bcf,0x0008,0x73ca,0x610c,0x2b00,0x5000,0x3b8e,0x
                       0x2083,0x30c4,0x4106,0x5148,0x6189,0x79cb,0x1001,0x2042,0x3044,0x4085,0x5086,0x60c8,0x78c9,0x1001,0x2002,0x3003,
                       0x4004,0x5005,0x6006,0x7807,0x1042,0x20c4,0x3106,0x4188,0x520a,0x624c,0x72cf,0x0842,0x1884,0x28c6,0x3908,0x494a,
                       0x598c,0x69cf,0x0802,0x1844,0x2846,0x3088,0x408a,0x50cc,0x60cf,0x0802,0x1804,0x2006,0x3008,0x400a,0x480c};
+*/
+
+uint16_t palette[] = {0x0000,0xFFFF,0x69AD,0x752E,0x69ED,0x5C6B,0x3146,0xBE37,0x6A6D,0x41C8,0x9B33,0x4228,0x6B6D,0x9E93,0x6AED,0x94B2};
+
+uint16_t mem_addrs[]={0x8000,0x8400,0x8800,0x8C00,0x9000,0x9400,0x9800,0x9C00,0x0000,0x0400,0x0800,0x0c00,0x1000,0x1400,0x1800,0x1C00};
+uint16_t color_addrs[]={0x9400,0x9600};
+
+uint32_t pal[]={0x000000,0xFFFFFF,0x68372B,0x70A4B2,0x6F3D86,0x588D43,0x352879,0xB8C76F,0x6F4F25,0x433900,0x9A6759,0x444444,0x6C6C6C,0x9AD284,0x6C5EB5,0x959595};
+
+// internal registers expanded
+uint8_t interlaced;
+uint8_t h_origin = 0x05;  //NTSC PAL:0x0C
+uint8_t v_origin = 0x19;  //NTSC PAL:0x26
+uint8_t screen_mem_offset;
+uint16_t raster_value;
+uint8_t columns=23;
+uint8_t rows=22;
+uint8_t double_size;
+uint16_t screenmem_loc = 0x1000;
+uint16_t charmem_loc = 0x8000;
+uint16_t colmem_loc = 0x9400;
+uint8_t aux_color;
+uint8_t reverse_mode;
+uint8_t screen_color;
+uint8_t border_color;
+
 
 struct fb_var_screeninfo screen_info;
 struct fb_fix_screeninfo fixed_info;
@@ -61,30 +88,28 @@ static inline void _point(uint16_t x, uint16_t y, uint8_t col)
     *((uint16_t*) (fbbuffer + location+fixed_info.line_length+screen_info.bits_per_pixel/8)) = pixel;
 }
 
-static void *display_thread()
+static void *display_thread(void *arg)
 {
   uint16_t g,h,i,j;
   uint8_t buf, c_rom;
 
-  while (1) {
-    for (g=0; g<23; g++) {
-      for (h=0; h<22; h++) {
+  while (0) {
+    for (g=0; g<rows; g++) {
+      for (h=0; h<columns; h++) {
         //
-        buf = mem[VID_MEMSTART+(22*g)+h];
+        buf = mem[screenmem_loc+(columns*g)+h];
 
         for (i=0; i<8; i++) {
-          c_rom=mem[CHAR_ROMSTART+(8*buf)+i];
+          c_rom=mem[charmem_loc+(8*buf)+i];
         
           for (j=0; j<8; j++) {
             if (c_rom&(1<<j)) {
               //setpixel
-    //	        _point(((10+g*8)+i)*SCREEN_WIDTH +70+(h*8)+8-j,0x00);
                 _point( 70+(h*8)+8-j,10+(g*8)+i,0x00);
             }
             else {
                 //clearpixel 
                 _point( 70+(h*8)+8-j,10+(g*8)+i,0x01);
-    //	        _point(((10+g*8)+i)*SCREEN_WIDTH+70+(h*8)+8-j,0x01);
             }
           }
         }
@@ -93,6 +118,110 @@ static void *display_thread()
 
     usleep(25000);
   }
+
+  uint8_t ntsc_screen_width = 210;
+  uint8_t ntsc_screen_height = 233;
+//  uint8_t pal_screen_width = 233;
+//  uint8_t pal_screen_height = 284;
+
+  uint16_t current_line = 0;
+  uint16_t current_column = 0;
+
+  //ntsc color screen draw
+  while(1) {
+    uint8_t x_border = h_origin<<2;
+    uint8_t y_border = v_origin;
+
+    uint16_t h_screen_pos = 0;  //pos in text screen
+    uint16_t v_screen_pos = 0;
+
+    current_line = 0;
+    current_column = 0;
+
+    for (current_line;current_line<y_border;current_line++) {
+      for (current_column=0; current_column < ntsc_screen_width; current_column++)
+        _point( current_column, current_line, border_color);
+    }
+
+    if ( double_size) {
+      //later
+    } else {
+      //normale size chars
+      for ( v_screen_pos = 0 ; v_screen_pos < rows; v_screen_pos++) {
+        // draw 8 lines
+        for (uint8_t h=0; h<8; h++) {
+
+          //draw border
+          for (current_column=0; current_column < x_border; current_column++)
+            _point( current_column, current_line, border_color);
+
+          for ( h_screen_pos=0; h_screen_pos < columns; h_screen_pos++) {
+            uint8_t thischar  = mem[screenmem_loc + (v_screen_pos*columns+h_screen_pos)];
+            uint8_t col_ram = mem[colmem_loc + v_screen_pos*columns+h_screen_pos];
+            uint8_t c_rom = mem[charmem_loc + (thischar<<3) + h];
+            
+            if (col_ram & 0x08) {
+              //Multicolor mode
+              uint8_t pixels[]={(c_rom & 0xC0)>>6, (c_rom & 0x30)>>6, (c_rom & 0x0C)>>6, (c_rom & 0x03)>>6 };
+              uint8_t pcols[]={screen_color,border_color,col_ram & 0x07,aux_color};
+
+              _point( current_column++, current_line, pcols[pixels[0]]);
+              _point( current_column++, current_line, pcols[pixels[0]]);
+
+              _point( current_column++, current_line, pcols[pixels[1]]);
+              _point( current_column++, current_line, pcols[pixels[1]]);
+
+              _point( current_column++, current_line, pcols[pixels[2]]);
+              _point( current_column++, current_line, pcols[pixels[2]]);
+
+              _point( current_column++, current_line, pcols[pixels[3]]);
+              _point( current_column++, current_line, pcols[pixels[3]]);
+
+            } else {
+              //Singlecolor
+              uint8_t pcols[] = {screen_color,col_ram & 0x7};
+
+               _point( current_column++, current_line, pcols[((c_rom&1<<7) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<6) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<5) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<4) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<3) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<2) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<1) != 0)]);
+               _point( current_column++, current_line, pcols[((c_rom&1<<0) != 0)]);
+/*              
+              for (j=7; j>0; j--) {
+                if (c_rom&(1<<j)) {
+                  //setpixel
+                    _point( current_column, current_line, pcol);
+                }
+                else {
+                    //clearpixel 
+                    _point( current_column, current_line, screen_color);
+                }
+                current_column++; 
+              } */
+            }
+          }
+
+          for (current_column; current_column < ntsc_screen_width; current_column++)
+            _point( current_column, current_line, border_color);
+
+          current_line++;
+        }
+      }
+    }
+
+    for (current_line; current_line<ntsc_screen_height; current_line++) {
+      for (current_column=0; current_column < ntsc_screen_width; current_column++)
+        _point( current_column, current_line, border_color);
+    }
+
+    usleep(25000);
+
+
+  }
+
 }
 
 int fb_init()
@@ -117,9 +246,7 @@ int fb_init()
               write(consolefd,CHAR_CURSOR_HIDE,6);
               close(consolefd);
             }
-
-            mem[VID_MEMSTART] = 55; 
-
+ 
             return 0;   /* Indicate success */
          }
          else {
@@ -145,7 +272,7 @@ int fb_init()
 // Basic info
 const char* name()
 {
-    return "basic framebuffer";
+    return "Commodore VIC framebuffer";
 }
 
 void mon_init(uint16_t base_addr, void *mon_mem, uint8_t *mon_interrupt)
@@ -163,6 +290,15 @@ void mon_init(uint16_t base_addr, void *mon_mem, uint8_t *mon_interrupt)
   if (pthread_create(&videoThread, NULL, display_thread, NULL)) {
       printf("kb thread create failed\n");
       exit(1);
+  }
+
+  for(int i=0;i<16;i++) {
+    uint8_t r = pal[i] >> 16;
+    uint8_t g = (pal[i] >> 8)&0xff;
+    uint8_t b = (pal[i] >> 16)&0xff;
+    
+      printf("0x%04X,",((r>>3)<<11 | (g>>2)<<5 | (b>>3)));
+      printf("\r\n");
   }
 
 }
@@ -185,10 +321,62 @@ uint8_t mon_read(uint16_t address)
   return mem[address];
 }
 
+
 // mem write
 void mon_write(uint16_t address, uint8_t data)
 {
+  address &= 0x0f;
+
+  switch(address) {
+    case 0:
+            interlaced = data & 0x80;
+            h_origin   = data & 0x7F;
+            printf("h_origin  : %04X\r\n",h_origin);
+            break;
+    case 1: 
+            v_origin = data;
+            printf("v_origin  : %04X\r\n",v_origin);
+            break;
+    case 2: 
+            screen_mem_offset = data & 0x80;
+            colmem_loc=color_addrs[screen_mem_offset>>7];
+            columns = data & 0x7f;
+            printf("color_mem: %04X\r\n",colmem_loc);
+            printf("columns   : %04X\r\n",columns);
+            break;
+    case 3:
+            raster_value |= (data & 0x80) >> 7;
+            rows = (data & 0x7e) >> 1;
+            double_size = data & 0x01;
+            printf("rows      : %04X\r\n",rows);
+            break;
+    case 4:
+            raster_value = data << 1;
+            break;
+    case 5:
+            screenmem_loc = mem_addrs[(data & 0xf0) >> 4];
+            charmem_loc = mem_addrs[data & 0x0f];
+            printf("screen_mem: %04X\r\n",screenmem_loc);
+            printf("char_mem:   %04X\r\n",charmem_loc);
+            break;
+    case 0x0e:
+            aux_color = (data & 0xf0) >> 4;
+            printf("aux_color: %04X\r\n",aux_color);
+            break;
+    case 0x0f:
+            screen_color = (data & 0xf0) >> 4;
+            reverse_mode = (data & 0x8);
+            border_color = (data & 0x7);
+            printf("screen_color: %04X\r\n",screen_color);
+            printf("border_color: %04X\r\n",border_color);
+            break;
+
+    default:
+            break;
+  }
+
   mem[address] = data;
+
 }
 
 uint8_t mon_do_tick(uint8_t ticks)
